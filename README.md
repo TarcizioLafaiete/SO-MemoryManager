40250ba0b56258a3e3fbdcaf0ce1745184a0b192
TarcizioLafaiete <tarcizio-agusto@hotmail.com>
2023-11-15 11:02:36 -0300

    Inicializando repositorio

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # SO-MemoryManager
+Repositorio destinado para o TP2 da disciplina de Sistemas Operacionais da UFMG
+
---
+++ mempager/src/pager.c
@@ -0,0 +1,37 @@
+#include "pager.h"
+#include <stdlib.h>
+
+typedef struct{
+    int* frames;
+    int* blocks;
+} pager_table;
+
+pager_table pages;
+
+void pager_init(int nframes, int nblocks){
+    pages.frames = (int*) malloc(sizeof(int) * nframes);
+    pages.blocks = (int*) malloc(sizeof(int) * nblocks);
+    for(int i = 0; i < nframes;i++){
+        pages.frames[i] = 0;
+    }
+    for(int i = 0; i < nblocks; i++){
+        pages.blocks[i] = 0;
+    }
+}
+
+void *pager_extend(pid_t pid){
+
+}
+
+void pager_fault(pid_t pid, void *addr){
+
+}
+
+int pager_syslog(pid_t pid, void *addr, size_t len){
+
+    return 0;
+}
+
+void pager_destroy(pid_t pid){
+
+}
