bd7368aa1c5530f12d53e622424316adf29b1ba4
TarcizioLafaiete <tarcizio-agusto@hotmail.com>
2023-11-17 00:02:47 -0300

    Testes ate o 7 funcionam, porem ainda n temos algoritmo de segunda chance

--- mempager/src/pager.c
+++ mempager/src/pager.c
@@ -8,6 +8,7 @@
 // Dados do tamanho de paginas e numero delas na regiao de memoria definida
 #define PAGE_SIZE sysconf(_SC_PAGESIZE)
 #define NUM_PAGES (UVM_MAXADDR - UVM_BASEADDR + 1) / PAGE_SIZE
+#define NO_ALLOC 0
 
 //Conversao de um endenreco virtual para um index de um vector (Utilizado para falicitar os acessos)
 #define VIRTAL_ADDR_TO_INDEX(vaddr) (int) (((int)vaddr - UVM_BASEADDR)/PAGE_SIZE)
@@ -16,8 +17,13 @@
 //Aqui deve-se adicionar possiveis bits e sinais uteis para as paginacoes
 typedef struct{
     short page_fault;
+    short in_frame;
     short write;
 } bytes_frames;
+
+typedef struct{
+    int alloc;
+} virtual_memory_map;
 
 // Celula, linka um pid a um endereco virtual
 typedef struct{
@@ -35,6 +41,7 @@ typedef struct{
 
 resource_t frame_t;
 resource_t block_t;
+virtual_memory_map* v_map;
 
 void init_table(resource_t *table){
     
@@ -42,26 +49,49 @@ void init_table(resource_t *table){
     // isto facilitara os nossos acessos nos algoritmos do page_fault
     for(int i = 0 ;i < table->size; i++){
         table->resource[i].pid = -1;
-        table->resource[i].vaddr = INDEX_TO_VIRTUAL_ADDR(i);
+        table->resource[i].vaddr = NO_ALLOC;
         table->resource[i].features.page_fault = 0;
+        table->resource[i].features.in_frame = 0;
         table->resource[i].features.write = 0;
     }
 }
 
-int alloc_pid(resource_t *table,pid_t pid){
-    for(int i = 0; i < table->size;i++){
+void init_memory_map(){
+    v_map = (virtual_memory_map*) malloc(sizeof(virtual_memory_map) * NUM_PAGES);
+    for(int i = 0; i < NUM_PAGES; i++){
+        v_map[i].alloc = 0;
+    }
+}
+
+void* alloc_pid(resource_t *table,pid_t pid){
+    void* addr;
+    for(int i = 0; i < NUM_PAGES; i++){
+        if(!v_map[i].alloc){
+            v_map[i].alloc = 1;
+            addr = INDEX_TO_VIRTUAL_ADDR(i);
+            break;
+        }
+   }
+
+    for(int i = 0; i < table->size; i++){
         if(table->resource[i].pid == -1){
             table->resource[i].pid = pid;
-            return 1;
+            table->resource[i].vaddr = addr;
         }
     }
-    return 0;
+
+    return addr;
+
 }
 
 void disalloc_pid(resource_t *table, pid_t pid){
     for(int i = 0; i < table->size;i++){
         if(table->resource[i].pid == pid){
+            void* addr = table->resource[i].vaddr;
+            int idx = VIRTAL_ADDR_TO_INDEX(addr);
+            v_map[idx].alloc = 0;
             table->resource[i].pid = -1;
+            table->free_cell++;
         }
     }
 }
@@ -76,14 +106,16 @@ void pager_init(int nframes, int nblocks){
     
     init_table(&frame_t);
     init_table(&block_t);
+
+    init_memory_map();
 }
 
 void pager_create(pid_t pid){
 
     // Ta basicao por equanto mas talvez precise de mais coisas no futuro
-    if(alloc_pid(&frame_t,pid) == 0){
-        exit(EXIT_FAILURE);
-    }
+    // if(alloc_pid(&frame_t,pid) == 0){
+    //     exit(EXIT_FAILURE);
+    // }
 
 }
 
@@ -93,38 +125,54 @@ void *pager_extend(pid_t pid){
     if(block_t.free_cell == 0){
         return NULL;
     }
-    alloc_pid(&block_t,pid);
+    void* addr = alloc_pid(&block_t,pid);
     block_t.free_cell--;
-
-    for(int i = 0; i < frame_t.size; i++){
-        if(frame_t.resource[i].pid == pid){
-            frame_t.free_cell--; //Isto aqui vai ser util na hora do pageFault confia em mim
-            return frame_t.resource[i].vaddr;
-        }
-    }
-
+    return addr;
 }
 
 void pager_fault(pid_t pid, void *addr){  
     
-    int frame_id = VIRTAL_ADDR_TO_INDEX(addr);
+    int idx = VIRTAL_ADDR_TO_INDEX(addr);
     //Primeiro acesso deve-se zerar os bits e a alocar espaco com permissao de leitura
-    if(frame_t.resource[frame_id].features.page_fault == 0){
-        frame_t.resource[frame_id].features.page_fault = 1;
-        mmu_zero_fill(frame_id);
-        mmu_resident(pid,addr,frame_id,PROT_READ);
+    if(block_t.resource[idx].features.page_fault == 0){
+        block_t.resource[idx].features.page_fault = 1;
+        block_t.resource[idx].features.in_frame = 1;
+        
+        frame_t.free_cell--;
+        frame_t.resource[frame_t.free_cell] = block_t.resource[idx];
+        
+        mmu_zero_fill(idx);
+        mmu_resident(pid,addr,idx,PROT_READ);
     }
     //Proximos acessos necessitam ter a sua permissao alterada para escrita e leitura
     else{ 
         //Evita trocas desnecessarias de permissao
-        if(frame_t.resource[frame_id].features.write == 0){
-            frame_t.resource[frame_id].features.write = 1;
+        if(block_t.resource[idx].features.write == 0 && block_t.resource[idx].features.in_frame == 1){
+            block_t.resource[idx].features.write = 1;
             mmu_chprot(pid,addr,PROT_WRITE | PROT_READ);
         }
     }
 }
 
 int pager_syslog(pid_t pid, void *addr, size_t len){
+    
+    if(addr < UVM_BASEADDR || addr > UVM_MAXADDR){
+        return -1;
+    }
+    
+    int index = VIRTAL_ADDR_TO_INDEX(addr);
+    if(block_t.resource[index].pid != pid){
+        return -1;
+    }
+    
+    void* vaddr = INDEX_TO_VIRTUAL_ADDR(index);
+    
+    unsigned char* buf = (unsigned char*) malloc(sizeof(unsigned char)*len);
+    buf = pmem + (index*PAGE_SIZE) + (addr - vaddr);
+    for(int i = 0; i < len;i++){
+        printf("%02x", (unsigned)buf[i]);
+    }
+    printf("\n");
     return 0;
 }
 
